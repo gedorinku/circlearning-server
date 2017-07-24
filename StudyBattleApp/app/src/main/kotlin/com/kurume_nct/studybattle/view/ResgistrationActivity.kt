b79b0bfadb6a55eefa3f56685d95023c65a6a52d
DESKTOP-34ARI2H\hanah <hanahanahunachi@gmail.com>
2017-07-24 14:39:01 +0900

    ui of registration is ok

--- StudyBattleApp/app/src/main/kotlin/com/kurume_nct/studybattle/view/ResgistrationActivity.kt
+++ StudyBattleApp/app/src/main/kotlin/com/kurume_nct/studybattle/view/ResgistrationActivity.kt
@@ -2,6 +2,9 @@ package com.kurume_nct.studybattle.view
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.renderscript.ScriptGroup
+import android.text.InputType
+import android.text.method.PasswordTransformationMethod
 import android.view.Gravity
 import android.widget.*
 import com.kurume_nct.studybattle.R
@@ -15,10 +18,8 @@ class RegistrationActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //setContentView(R.layout.activity_registrarion)
         RegistrationActivityUI().setContentView(this)
     }
-
 }
 
 class RegistrationActivityUI : AnkoComponent<RegistrationActivity>{
@@ -29,16 +30,18 @@ class RegistrationActivityUI : AnkoComponent<RegistrationActivity>{
             val name = editText {
                 hint = "UserName"
             }
-            val password = editText{
+            val password = editText {
                 hint = "Password"
+                inputType = InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_PASSWORD
             }
 
             button("登録"){
                 onClick {
-                    if(name == null || password != null){
-                        //toast("入力に不備があるようです.")
+                    if(name.text.isEmpty() || password.text.isEmpty()){
+                        toast("入力に不備があるようです.")
                     }
                     else{
+                        toast("登録完了しました")
                         //startActivity<>()
                     }
                 }
