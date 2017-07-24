ed24b94d8f2bc37ce65290941ef160bbd681ac0c
gedorinku <gedorinku@yahoo.co.jp>
2017-07-24 15:42:43 +0900

    ログインのテストを書いた。

--- StudyBattleServer/src/test/kotlin/com/kurume_nct/studybattleserver/StudyBattleServerAppTest.kt
+++ StudyBattleServer/src/test/kotlin/com/kurume_nct/studybattleserver/StudyBattleServerAppTest.kt
@@ -1,8 +1,17 @@
 package com.kurume_nct.studybattleserver
 
+import io.netty.handler.codec.http.multipart.InterfaceHttpData
+import org.jetbrains.ktor.application.Application
+import org.jetbrains.ktor.http.formUrlEncode
+import org.jetbrains.ktor.http.HttpHeaders
+import org.jetbrains.ktor.http.HttpMethod
+import org.jetbrains.ktor.http.HttpStatusCode
+import org.jetbrains.ktor.testing.handleRequest
+import org.jetbrains.ktor.testing.withTestApplication
+import org.jetbrains.ktor.util.*
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNotEquals
 import org.junit.Test
-
-import org.junit.Assert.*
 import java.security.MessageDigest
 import java.security.SecureRandom
 import javax.xml.bind.DatatypeConverter
@@ -11,6 +20,18 @@ import javax.xml.bind.DatatypeConverter
  * Created by gedorinku on 2017/07/23.
  */
 class StudyBattleServerAppTest {
+
+    @Test
+    fun loginTest() = withTestApplication(Application::studyBattleServerApp) {
+        val values = listOf("userName" to "test", "password" to "hogehoge")
+        with (handleRequest(HttpMethod.Post, "/login") {
+            addHeader(HttpHeaders.ContentType, "application/x-www-form-urlencoded")
+            body = values.formUrlEncode()
+        }) {
+            assertEquals(HttpStatusCode.OK, response.status())
+        }
+    }
+
     @Test
     fun hashWithSaltTest() {
         val password = "password123"
