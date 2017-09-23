2c7e1ab2d75881e9a0ad882d411a45848e7bfda4
Kirby <takasu55220@gmail.com>
2017-09-23 17:53:50 +0900

    一通りの流れを作成（解答提出～アイテム抽選画面）

--- StudyBattleApp/app/src/main/java/com/example/owner/myapplicationipracticenowin201709/CameraModeActivity.java
+++ StudyBattleApp/app/src/main/java/com/example/owner/myapplicationipracticenowin201709/CameraModeActivity.java
@@ -1,8 +1,8 @@
 package com.example.owner.myapplicationipracticenowin201709;
 
+import com.kurume_nct.studybattle.R;
 import android.app.Activity;
 import android.graphics.Bitmap;
-import android.graphics.Interpolator;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
@@ -15,7 +15,7 @@ import android.net.Uri;
 import android.os.ParcelFileDescriptor;
 import android.util.Log;
 import android.widget.TextView;
-
+import android.os.Handler;
 import java.io.FileDescriptor;
 import java.io.IOException;
 
@@ -28,20 +28,28 @@ public class CameraModeActivity extends Activity {
 
     private ImageView imageview;
     private Button libraryButton;
-    private TextView dcimPath;
+    private TextView commnent_;
+    private ImageView shock;
+    private int flag=0;
+
 
     @Override
     protected void onCreate(Bundle saveInstanceState) {
         super.onCreate(saveInstanceState);
         setContentView(R.layout.activity_camera_mode);
-
-        dcimPath = (TextView) findViewById(R.id.text_view2);
-        //ギャラリーのpathを取得する
-        dcimPath.setText("ギャラリーのpath: " + getGalleryPath());
-
-        imageview = (ImageView) findViewById((R.id.image_view));
-
+        // 宣言
+        commnent_ = (TextView) findViewById(R.id.comment);
+        //commnent_.setText("ギャラリーのpath: " + getGalleryPath());//ギャラリーのpathを取得する
+        imageview = (ImageView) findViewById((R.id.answer));//解答の写真
         libraryButton = (Button) findViewById(R.id.library_button);
+        final Button submissionButton=(Button) findViewById(R.id.submission_button);
+        final Button returnButton = (Button) findViewById(R.id.return_button1);
+        Button passButton=(Button) findViewById(R.id.pass_button);
+        shock=(ImageView) findViewById(R.id.shock_character);
+
+        shock.setVisibility(View.INVISIBLE);
+
+
         libraryButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -55,8 +63,8 @@ public class CameraModeActivity extends Activity {
             }
         });
 
-
-        Button cameraButton = (Button) findViewById(R.id.camera_button);
+        //カメラ撮影ボタン
+        final Button cameraButton = (Button) findViewById(R.id.camera_button);
         cameraButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -65,8 +73,7 @@ public class CameraModeActivity extends Activity {
             }
         });
 
-
-        Button returnButton = (Button) findViewById(R.id.return_button1);
+        //戻るボタン
         returnButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -74,22 +81,55 @@ public class CameraModeActivity extends Activity {
             }
         });
 
-        Button passButton=(Button) findViewById(R.id.pass_button);
-        cameraButton.setOnClickListener(new View.OnClickListener() {
+        //パスするボタン
+        passButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent1 = new Intent(getApplication(),LottelyActivity.class);
-                startActivity(intent1);
+
+                //他の画面に遷移しないようにする
+                returnButton.setEnabled(false);
+                submissionButton.setEnabled(false);
+                cameraButton.setEnabled(false);
+                libraryButton.setEnabled(false);
+
+                commnent_.setText("次は頑張ろう.....");
+                shock.setVisibility(View.VISIBLE);
+                Bitmap bitmap=BitmapFactory.decodeResource(getResources(),R.drawable.shock);
+                shock.setImageBitmap(bitmap);
+
+
+                final Handler handler=new Handler();
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        finish();
+                    }
+                },2500);
+            }
+        });
+
+        //提出するボタン
+        submissionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(flag==1) {
+                    Intent intent1 = new Intent(getApplication(), LotteryActivity.class);
+                    startActivity(intent1);
+                }else commnent_.setText("解答を提出してください");
             }
         });
     }
 
+
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == RESULT_CAMERA) {
+            //カメラ撮影の処理
             Bitmap bitmap = (Bitmap) data.getExtras().get("data");
             imageview.setImageBitmap(bitmap);
         }else if (requestCode == RESULT_PICK_IMAGEFILE && resultCode == Activity.RESULT_OK) {
+            //ギャラリーからの写真を選択
             Uri uri=null;
             if (data!= null) {
                 uri= data.getData();
@@ -103,6 +143,8 @@ public class CameraModeActivity extends Activity {
                 }
             }
         }
+        flag=1;
+        commnent_.setText("解答を提出してね");
     }
 
     //ギャラリーpath取得関数
@@ -110,6 +152,7 @@ public class CameraModeActivity extends Activity {
         return Environment.getExternalStorageDirectory() + "/" + Environment.DIRECTORY_DCIM + "/";
     }
 
+    //画像を選んで取得して返す？（多分）
     private Bitmap getBitmapFromUri(Uri uri) throws IOException {
         ParcelFileDescriptor parcelFileDescriptor =
                 getContentResolver().openFileDescriptor(uri, "r");
