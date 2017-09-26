f0a66461e574189eef55ee998559c4f81638f884
gedorinku <gedorinku@yahoo.co.jp>
2017-09-26 19:49:15 +0900

    androidのプロジェクトを削除。

--- StudyBattleApp/app/src/main/kotlin/com/kurume_nct/studybattle/model/Person_Group.kt
+++
@@ -1,6 +0,0 @@
-package com.kurume_nct.studybattle.model
-
-/**
- * Created by hanah on 9/22/2017.
- */
-data class Person_Group(var name : String = "gedohusa", var id : Int = 0, var icon_id : Int = 0)
