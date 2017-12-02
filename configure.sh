8df93ead59aed573d1cd738d732ba24cbbe14719
gedorinku <gedorinku@yahoo.co.jp>
2017-12-02 12:59:51 +0900

    ビルド用のスクリプトを修正。

---
+++ configure.sh
@@ -0,0 +1,3 @@
+cp ~/study-battle-server/StudyBattleServer/database.properties.sample ~/study-battle-server/StudyBattleServer/database.properties
+cat ~/study-battle-server/db_setup | mysql -u root
+
