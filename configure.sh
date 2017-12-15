e3d9a17e8a89bd176148a82ec5524e4897274c84
gedorinku <gedorinku@users.noreply.github.com>
2017-12-15 18:06:36 +0900

    typo修正。

--- configure.sh
+++ configure.sh
@@ -1,3 +1,3 @@
-cp ~/circlearning-server/CirclearningServer/database.properties.sample ~/circlearing-server/CirclearningServer/database.properties
+cp ~/circlearning-server/CirclearningServer/database.properties.sample ~/circlearning-server/CirclearningServer/database.properties
 cat ~/circlearning-server/db_setup | mysql -u root
 
